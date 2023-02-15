491be18a744c61d1129512043de67567d0d5a4ca
Yauheniya Seviarynets <yauheniya_seviarynets@epam.com>
2023-02-15 03:01:45 +0100

    Add header according to PR comments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,11 @@ import styles from './Header.module.css';
 import { Logo } from './components/Logo/Logo';
 import { Login } from './components/Login/Login';
 
-const Header = () => {
-	return (
-		<header className={styles.header}>
-			<Logo />
-			<Login />
-		</header>
-	);
-};
+const Header = () => (
+	<header className={styles.header}>
+		<Logo />
+		<Login />
+	</header>
+);
 
 export default Header;
